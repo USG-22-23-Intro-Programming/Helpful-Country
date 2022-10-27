a41887ff526f1078bb4b715f061356b41accbe2d
NawalAhmad <113056962+NawalAhmad@users.noreply.github.com>
2022-10-27 11:16:55 -0700

    Add files via upload

---
+++ PythonProblemSet#2.py
@@ -0,0 +1,41 @@
+#Factorial!
+def factorial(x):
+    y = 1
+    for i in range (1, x + 1):
+        #^only gonna inlcude number 1 - x+1, not inlcuding the answer for x+1 and 1 just numbers in between
+        y = y * i
+
+    print (" The factorial of " + str(x) + " is ")
+    print (y)
+
+#Double It!
+def double():
+    double = input("What do word do you want to double?")
+    output = ""
+    #^ the quotations marks are just there to just keep the whole word together, compliling them
+    for char in double:
+        output = output + char * 2
+    print(output)
+
+#Camel Case!
+def 
+
+        
+    
+def main():
+    factorial(6)
+    factorial(13)
+
+
+if __name__ == "__main__":
+    main()
+    double()
+
+
+    
+    
+    
+    
+    
+    
+    
